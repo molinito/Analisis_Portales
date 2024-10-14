9fda1843e3a53fab01797b699b4ad95888525401
molinito <molinito48@gmail.com>
2024-10-14 15:13:16 -0300

    vitearreglado

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Carpeta de salida para los archivos de producción
+  },
   server: {
     proxy: {
-      "/api": "http://localhost:3005",
+      "/api": "http://localhost:3005", // Solo en desarrollo
     },
   },
 });
