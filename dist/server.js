9e38a48924e8c42e7a4c59e3c8b2e16034769c50
molinito <molinito48@gmail.com>
2024-10-14 15:27:46 -0300

    server

--- dist/server.js
+++ dist/server.js
@@ -14,12 +14,12 @@ const port = 3005;
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 // Sirve los archivos estáticos del frontend compilado
-app.use(express_1.default.static(path_1.default.join(__dirname, "../frontend/build")));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../frontend/dist")));
 // API routes
 app.use("/api", routes_1.router);
 // Para cualquier ruta que no sea de API, servir el frontend
 app.get("*", (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "../frontend/build", "index.html"));
+    res.sendFile(path_1.default.join(__dirname, "../frontend/dist", "index.html"));
 });
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
